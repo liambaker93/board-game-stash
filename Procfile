5fc8e60748844fb6e92893687b0dd0857b4578a2
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-30 17:39:44 +0100

    add heroku link

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn board_game_list.wsgi
--- board_game_stash/settings.py
+++ board_game_stash/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yl4quzkh-(6u66^ir8d06_0xa47y4yr==!9)@j(90zz5hrwe+c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '.127.0.0.1',]
 
 
 # Application definition
